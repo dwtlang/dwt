feacaeee48076de9ff34c1d1cd9d3f73aac3f3e9
Andrew Scott-Jones <andrew@dwtscript.org>
2020-12-13 15:53:39 +0000

    Benchmark LuaJIT with JIT disabled for more useful comparison

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 g++ -O3 fib.c -ofib
-/usr/bin/time -f "c,%e" ./fib 2> results.csv 1>/dev/null
-/usr/bin/time -f "dwt,%e" dwt fib.dwt >> results.csv 2>&1
-/usr/bin/time -f "luajit,%e" luajit fib.lua >> results.csv 2>&1
-/usr/bin/time -f "lua,%e" lua fib.lua >> results.csv 2>&1
-/usr/bin/time -f "ruby,%e" ruby fib.rb >> results.csv 2>&1
-/usr/bin/time -f "python,%e" python fib.py >> results.csv 2>&1
+/usr/bin/time -f "C,%e" ./fib 2> results.csv 1>/dev/null
+/usr/bin/time -f "Dwt,%e" dwt fib.dwt >> results.csv 2>&1
+/usr/bin/time -f "LuaJIT (off),%e" luajit -joff fib.lua >> results.csv 2>&1
+/usr/bin/time -f "Lua,%e" lua fib.lua >> results.csv 2>&1
+/usr/bin/time -f "Ruby,%e" ruby fib.rb >> results.csv 2>&1
+/usr/bin/time -f "Python,%e" python fib.py >> results.csv 2>&1
 
 python plot.py
 
